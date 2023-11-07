41aaefb734ddb66584a39ce98937b74dbf8a0815
Adam McNeilly <amcneilly331@gmail.com>
2023-11-07 15:14:08 -0500

    Formatting.

--- app/src/main/java/com/adammcneilly/tcgdex/api/TCGAPIHeaderInterceptor.kt
+++ app/src/main/java/com/adammcneilly/tcgdex/api/TCGAPIHeaderInterceptor.kt
@@ -5,7 +5,6 @@ import okhttp3.Interceptor
 import okhttp3.Response
 
 class TCGAPIHeaderInterceptor : Interceptor {
-
     override fun intercept(chain: Interceptor.Chain): Response {
         val requestBuilder = chain.request().newBuilder()
 
--- app/src/main/java/com/adammcneilly/tcgdex/api/TCGRetrofitAPI.kt
+++ app/src/main/java/com/adammcneilly/tcgdex/api/TCGRetrofitAPI.kt
@@ -1,6 +1,5 @@
 package com.adammcneilly.tcgdex.api
 
-import com.adammcneilly.tcgdex.BuildConfig
 import com.adammcneilly.tcgdex.api.dto.TCGCardListDTO
 import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
@@ -10,7 +9,6 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 interface TCGRetrofitAPI {
-
     @GET("/v2/cards")
     suspend fun getCards(): TCGCardListDTO
 
@@ -20,14 +18,16 @@ interface TCGRetrofitAPI {
         fun defaultInstance(): TCGRetrofitAPI {
             val headerInterceptor = TCGAPIHeaderInterceptor()
 
-            val loggingInterceptor = HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            }
+            val loggingInterceptor =
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
 
-            val client = OkHttpClient.Builder()
-                .addInterceptor(headerInterceptor)
-                .addInterceptor(loggingInterceptor)
-                .build()
+            val client =
+                OkHttpClient.Builder()
+                    .addInterceptor(headerInterceptor)
+                    .addInterceptor(loggingInterceptor)
+                    .build()
 
             val moshi = Moshi.Builder().build()
 
